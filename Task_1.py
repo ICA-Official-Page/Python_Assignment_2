675d87e8dbb4816d5de9e501cc8dc61dd51922dd
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-09-20 12:27:19 +0530

    commit
    commit

---
+++ Task_1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the First Name: "))
+
+if a%2==0:
+    print("Number is Even.")
+else:
+    print("Number is Odd.")
---
+++ Task_2.py
@@ -0,0 +1,8 @@
+
+
+a=(list(range(1,51)))
+count=0
+for i in a:
+    count=count+i
+
+print("The Sum of Numbers from 1 to 50 is : ",count)
